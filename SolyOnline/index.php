6bc6eb7e43bc2c44e0b7f2bcb6604f152f54a5a8
Samir Eltabbal <soly.0@live.com>
2017-03-26 18:15:47 +0200

    Uploading First Theme Files
    Uploading the first theme ( Files for Tut #3 ) in Creating WordPress Theme

---
+++ SolyOnline/index.php
@@ -0,0 +1,4 @@
+<?php // this is index Page ?>
+<?php get_header();  // this function calls the header.php file ?>
+<h1>Hello World</h1>
+<?php get_footer();  // this function calls the footer.php file ?>
